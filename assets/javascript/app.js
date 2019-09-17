3a0d52dd25982a792e1cab7a5c5de14e79b1d3b9
Stephen McMillen <Shtevetm@gmail.com>
2019-09-17 16:36:12 -0500

    user-input functioning in queryURL var.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,24 @@
+// queryURL components//
 var protocol = "http://api.giphy.com/v1/gifs/search?q="
 var term = ""
 var query = "&lang:en&limit:10"
 var api = "&api_key=j7uGW8a9SseGiiroM6bZvYm3X40oKHSh"
-
+// queryURL var declared//
 var queryURL = protocol + term + query + api;
-console.log(queryURL);
+
+// function to trigger AJAX call using queryURL var//
+$("#findGif").on("click", function (event) {
+    event.preventDefault();
+    term = $("#userInput").val();
+    queryURL = protocol + term + query + api;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+        $("#gifView").val(response)
+    });
+    console.log(term);
+})
