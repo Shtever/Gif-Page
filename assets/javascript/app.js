73bf4585442a1f97584e93fbe9a6f408d970ca2d
Stephen McMillen <Shtevetm@gmail.com>
2019-09-17 22:32:56 -0500

    buttons generate on page with user inputs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,26 +11,35 @@ $("#findGif").on("click", function (event) {
     event.preventDefault();
     term = $("#userInput").val();
     queryURL = protocol + term + query + api;
-
-
+    createButton();
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        for (var i = 0; i <= 10; i++){
         console.log(queryURL);
         console.log(response);
-        for (var i = 0; i <= 10; i++)
         var newDiv = $("<div class='gif'>");
         var rating = "Rating: " + response.data[i].rating;
         var imgURL = response.data[i].images.fixed_height.url;
         var img = $("<img>").attr("src", imgURL);
         newDiv.append(img);
-   
-        $("#gifView").append(rating);
-        $("#gifImage").append(img);        
-        
-        // Removed JSON.stringify because the results had quotation marks around them//
+        prependGifs()
+        }
+        function prependGifs(){
+            $("#gifView").prepend(rating);
+            $("#gifImage").prepend(img); 
+          }
+        // JSON.stringify causes quotes around response elements//
         // $("#gifView").append(JSON.stringify("Rating: " + response.data[i].rating + "<img src=" + response.data[i].images.fixed_height.url + close));
     });
     console.log(term);
 })
+
+function createButton(){
+    var btn = document.createElement("button");
+    btn.innerHTML =$("#userInput").val();
+    $("#buttons").append(btn);
+  }
+
