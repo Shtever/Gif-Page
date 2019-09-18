bd4d2af12e45e9d28318deb5be1d195234d0ba41
Stephen McMillen <Shtevetm@gmail.com>
2019-09-17 19:05:37 -0500

    gifDiv populates 1 image and rating. Code is fucked.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
 // queryURL components//
-var close = ">"
 var protocol = "http://api.giphy.com/v1/gifs/search?q="
 var term = ""
-var query = "&lang:en&limit:10"
+var query = "&limit:10"
 var api = "&api_key=j7uGW8a9SseGiiroM6bZvYm3X40oKHSh"
 // queryURL var declared//
 var queryURL = protocol + term + query + api;
@@ -18,10 +17,18 @@ $("#findGif").on("click", function (event) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(queryURL);
         console.log(response);
         for (var i = 0; i <= 10; i++)
-
-        $("#gifView").append("Rating: " + response.data[i].rating + "<img src=" + response.data[i].images.fixed_height.url + close);
+        var newDiv = $("<div class='gif'>");
+        var rating = "Rating: " + response.data[i].rating;
+        var imgURL = response.data[i].images.fixed_height.url;
+        var img = $("<img>").attr("src", imgURL);
+        newDiv.append(img);
+   
+        $("#gifView").append(rating);
+        $("#gifImage").append(img);        
+        
         // Removed JSON.stringify because the results had quotation marks around them//
         // $("#gifView").append(JSON.stringify("Rating: " + response.data[i].rating + "<img src=" + response.data[i].images.fixed_height.url + close));
     });
