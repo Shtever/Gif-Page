212627e267ba615daeb391c8c7c22384e1169af2
Stephen McMillen <Shtevetm@gmail.com>
2019-09-17 23:02:35 -0500

    added JQuery in app.js to stop/animate gifs. Doesn't work...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,34 +12,51 @@ $("#findGif").on("click", function (event) {
     term = $("#userInput").val();
     queryURL = protocol + term + query + api;
     createButton();
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        for (var i = 0; i <= 10; i++){
-        console.log(queryURL);
-        console.log(response);
-        var newDiv = $("<div class='gif'>");
-        var rating = "Rating: " + response.data[i].rating;
-        var imgURL = response.data[i].images.fixed_height.url;
-        var img = $("<img>").attr("src", imgURL);
-        newDiv.append(img);
-        prependGifs()
+        for (var i = 0; i <= 10; i++) {
+            var newDiv = $("<div class='gif'>");
+            var rating = "Rating: " + response.data[i].rating;
+            var imgURL = response.data[i].images.fixed_height.url;
+            var img = $("<img>").attr("src", imgURL);
+            $("<img>").attr("data-still")
+            newDiv.append(img);
+            prependGifs()
         }
-        function prependGifs(){
+        function prependGifs() {
             $("#gifView").prepend(rating);
-            $("#gifImage").prepend(img); 
-          }
+            $("#gifView").prepend(img);
+            $(this).attr("src", $(this).attr("data-still"));
+            gifStop();
+        }
         // JSON.stringify causes quotes around response elements//
         // $("#gifView").append(JSON.stringify("Rating: " + response.data[i].rating + "<img src=" + response.data[i].images.fixed_height.url + close));
     });
     console.log(term);
 })
 
-function createButton(){
+function createButton() {
     var btn = document.createElement("button");
-    btn.innerHTML =$("#userInput").val();
+    btn.innerHTML = $("#userInput").val();
     $("#buttons").append(btn);
-  }
+}
 
+function gifStop() {
+    $("#gifImage").on("click", function () {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    })
+};
